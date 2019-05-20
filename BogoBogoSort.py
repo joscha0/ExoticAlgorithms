a554951b8355422acf57ef58576bbfcf2babf96d
joscha <joscha@fawer.de>
2019-05-21 00:46:45 +0200

    updated bogobogosort

--- BogoBogoSort.py
+++ BogoBogoSort.py
@@ -1,28 +1,22 @@
 import random
 
 def is_sorted(lst):
-    for i in range(len(lst) - 1):
-        if lst[i] > lst[i + 1]:
-            return False
-    return True
-
-def bogoSort(lst):
-    while not is_sorted(lst):
-        random.shuffle(lst)
-    return lst
+	cpy = lst[:]
+	std = bogoBogoSort(cpy[:-1])
+	while cpy[-1] < max(std):
+		random.shuffle(cpy)
+		std = bogoBogoSort(cpy[:-1])
+	return std == lst[:-1]
 
 def bogoBogoSort(lst):
-	index = 2
-	while(not is_sorted(lst)):
-		bogoSort(lst[:index])
-		index += 1
-		if(not is_sorted(lst[:index])):
-			random.shuffle(lst)
-			index = 2
+	if len(lst) == 1:
+		return lst
+	while not is_sorted(lst):
+		random.shuffle(lst)
 	return lst
 
 if __name__ == "__main__":
-    list1 = [1,4,2,5,6,5,3]
+    list1 = [7,1,8,4,3]
     print('\n\n-----Bogobogosort Sorting Algorithm-----\n')
     print('unsorted list:   '+str(list1))
     print('sorted list:     '+str(bogoBogoSort(list1)))
