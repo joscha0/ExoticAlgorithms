57c526587facba4ce8228c5325e09d5b18a92606
joscha <joscha@fawer.de>
2019-05-20 19:14:11 +0200

    added pick to run

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from  StalinSort import stalinSort
 from  slowSort import slowSort
 import numpy as np
 import time
+from pick import pick
 
 def time_it(func):
     start = time.time()
@@ -17,8 +18,18 @@ def time_it(func):
 
 algsList = [bogoBogoSort, bogoSort, bozoSort, communismSort, miracleSort, stalinSort, slowSort]
 
-for alg in algsList:
-    print('\n'+str(alg.__name__)+ '...')
+title = 'Please choose a algorithm: '
+options = ['bogoBogoSort', 'bogoSort', 'bozoSort', 'communismSort', 'miracleSort', 'stalinSort', 'slowSort', 'all']
+option, index = pick(options, title)
+
+if option == 'all':
+    for alg in algsList:
+        print('\n'+str(alg.__name__)+ '...')
+        randlist = np.random.randint(0, 100, 5).tolist()
+        print('unsorted list: ', randlist)
+        time_it(lambda: alg(randlist))
+else:
+    print('\n'+str(algsList[index].__name__)+ '...')
     randlist = np.random.randint(0, 100, 5).tolist()
     print('unsorted list: ', randlist)
-    time_it(lambda: alg(randlist))
+    time_it(lambda: algsList[index](randlist))
